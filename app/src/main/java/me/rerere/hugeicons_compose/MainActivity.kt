97f02a8bea906363be1c740ce148a953ce175b8b
re-ovo <1609403959@qq.com>
2026-03-04 14:23:55 +0800

    Add HugeIcons display in Greeting composable

--- app/src/main/java/me/rerere/hugeicons_compose/MainActivity.kt
+++ app/src/main/java/me/rerere/hugeicons_compose/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import me.rerere.hugeicons.HugeIcons
+import me.rerere.hugeicons.stroke.CallMinus
+import me.rerere.hugeicons.stroke.Earth
+import me.rerere.hugeicons.stroke.Searching
 import me.rerere.hugeicons_compose.ui.theme.HugeiconscomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +38,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+    ) {
+        Icon(HugeIcons.Earth, null)
+        Icon(HugeIcons.CallMinus, null)
+        Icon(HugeIcons.Searching, null)
+    }
 }
 
 @Preview(showBackground = true)
